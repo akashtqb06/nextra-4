92cfafdf9c174c9a18adf6c183ea3ee4aa33f356
Rajdeep Singh <radewala.2@gmail.com>
2025-01-22 19:09:30 +0530

    meta json

---
+++ src/app/_meta.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  index: {
+    display: 'hidden'
+  },
+  docs: {
+    type: 'page',
+    title: 'Documentation'
+  }
+}
