865300ee10b11d997cac2df698aaa1e0f636c9f7
Rajdeep Singh <radewala.2@gmail.com>
2025-01-22 19:10:37 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+import { NextConfig } from "next";
+
+import nextra from "nextra";
+
+const withNextra = nextra({
+  latex: true,
+  defaultShowCopyCode: true,
+  contentDirBasePath: "/docs",
+});
+
+const nextConfig: NextConfig = withNextra({
+  reactStrictMode: true,
+});
 
 export default nextConfig;
